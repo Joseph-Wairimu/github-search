d0a6e0e8d73f5a80927f0cc7b9aad785168253fc
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-12 23:11:59 +0300

    adding a redirect to homepage

--- github/Github/src/app/app-routing.module.ts
+++ github/Github/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchUserComponent } from './search-user/search-user.component';
 import { SearchRepositoryComponent } from './search-repository/search-repository.component';
 import { HomepageComponent } from './homepage/homepage.component';
-
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: ' Homepage', component:  HomepageComponent },
   { path: 'Search-User', component: SearchUserComponent },
+  { path:'**', component:NotFoundComponent},
+  { path: '', redirectTo:"/Homepage", pathMatch:"full"},
   { path: 'Search-Repository', component:  SearchRepositoryComponent},
 ];
 @NgModule({
