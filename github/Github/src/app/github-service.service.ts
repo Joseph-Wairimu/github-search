0ddf673d12d08196b43748292bf084528576d68b
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-12 12:19:49 +0300

    import httclient into services

--- github/Github/src/app/github-service.service.ts
+++ github/Github/src/app/github-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
-import { HttpClientModule } from '@angular/common/http';
+import {  HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
