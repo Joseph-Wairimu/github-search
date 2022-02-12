33ef467c6e33feac61290ccaf8f4b11ccfa55a73
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-12 22:37:01 +0300

    adding app routing

--- github/Github/src/app/app-routing.module.ts
+++ github/Github/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchUserComponent } from './search-user/search-user.component';
-
+import { SearchRepositoryComponent } from './search-repository/search-repository.component';
 const routes: Routes = [
   { path: 'Search-User', component: SearchUserComponent },
-  { path: 'about', component: AboutComponent},
+  { path: 'Search-Repository', component:  SearchRepositoryComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
