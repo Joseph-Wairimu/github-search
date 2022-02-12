7cc0b285fb77b2839f9d787a869025abd21267a4
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-12 11:49:05 +0300

    adding constructor to class user

--- github/Github/src/app/user.ts
+++ github/Github/src/app/user.ts
@@ -1,4 +1,16 @@
 export class User {
-   
+    constructor(
+        public name: string,
+        public avatar_url: string,
+        public blog: string,
+        public html_url: string,
+        public location: string,
+        public bio: string,
+        public public_repos: number,
+        public followers: number,
+        public following: number,
+        public created_at: Date,
+        public company?: string,
+      ) {}
 
 }
